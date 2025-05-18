2208a5b6ff214312de5efdfede2dfa232e7d1499
Luka Skenderija <luka.skenderija@gmail.com>
2025-05-18 02:05:10 +0200

    Setup input basics

--- Program.cs
+++ Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using lexer;
+
+class Program
+{
+    private static string EXECUTABLE_NAME = "lolcode";
+
+    private static void Usage()
+    {
+        Console.WriteLine($"Usage: {EXECUTABLE_NAME} [file]");
+    }
+
+    private static void Process(string sourceCode)
+    {
+        Lexer lexer = new(sourceCode);
+    }
+
+    public static void Main(string[] args)
+    {
+        if (args.Length == 2)
+        {
+            Process(args[1]);
+        }
+        else
+        {
+            Usage();
+        }
+    }
+}
